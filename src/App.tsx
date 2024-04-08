37c2fc6b1eb7470ea773c8418684d1d8777b9d71
Donghee Hong <dh.hong@skku.edu>
2024-04-09 03:07:12 +0900

    [ADD]

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeView from "./views/HomeView";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>hi</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
